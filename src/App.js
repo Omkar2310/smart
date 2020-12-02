ee056466abcc9cd28ed8c8e93a4b035f40ce197a
Omkar2310 <mankar.omkar2310@gmail.com>
2020-12-02 10:04:54 +0530

    updating the shop page with collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.components';
 import './App.css';
-
-const hatspage = () => (
-  <div>
-    <h1> Hi Omkar</h1>
-  </div>
-
-);
+ 
 
 class App extends React.Component {
   render() {
@@ -17,7 +11,7 @@ class App extends React.Component {
       <div className='App'>
         <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={hatspage}/>
+        <Route exact path='/shop' component={ShopPage}/>
         </Switch>
         {/* <HomePage /> */}
       </div>
